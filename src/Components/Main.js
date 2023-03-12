8e5bf615aa30d395afe3f81238050bedc82ddbd7
Ermiyas H <Ermi@Ermiyass-MacBook-Pro-2.local>
2023-03-11 18:38:11 -0800

    Main.js added

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,4 +1,69 @@
 import { useEffect, useState } from "react";
-import {Routes, Route} from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
+import Index from "../pages/Index";
+import Show from "../pages/Show";
 
+const Main = () => {
+    const [blogs, setBlogs] = useState([]);
+    
+    const url = `https://final-jwwb.onrender.com/blogs/`;
+    
+    const getBlogs = async () => {
+        const response = await fetch(url);
+        const data = await response.json();
+        setBlogs(data);
+    };
+    
+    const createBlogs = async (blog) => {
+        await fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "Application/json",
+        },
+        body: JSON.stringify(blog),
+        });
+        getBlogs();
+    };
+    
+    const updateBlogs = async (blog, id) => {
+        await fetch(url + id + "/", {
+        method: "put",
+        headers: {
+            "Content-Type": "Application/json",
+        },
+        body: JSON.stringify(blog),
+        });
+        getBlogs();
+    };
+    
+    const deleteBlogs = async (id) => {
+        await fetch(url  + id + "/", {
+        method: "delete",
+        });
+        getBlogs();
+    };
+    useEffect(() => { getBlogs() }, []);
+    return (
+        <main>
+        <Routes>
+            <Route 
+            exact path="/"
+            element={<Index
+            blogs={blogs} 
+            createBlogs={createBlogs} />} />
+            <Route
+            path="/blog/:id" element={<Show
+            blogs={blogs}
+            updateBlogs={updateBlogs}
+            deleteBlogs={deleteBlogs} />} />
+             {/* <Route
+            path="/write" element={<Show
+            blogs={blogs}
+            createBlogs={createBlogs} /> } /> */}
 
+        </Routes>
+        </main>
+    );
+}
+
+export default Main;
