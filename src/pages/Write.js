fe90ac98ce62341e9476360a4c70a20f4ea43a62
Ermiyas H <Ermi@Ermiyass-MacBook-Pro-2.local>
2023-03-13 00:04:44 -0700

    back to working app, issues fixed

--- src/pages/Write.js
+++ src/pages/Write.js
@@ -7,77 +7,77 @@ export default function Write({ createBlogs }) {
     author: "",
   });
 
-  const handleChange = (e) => {  // publish page
+  const handleChange = (e) => {
+    // publish page
     setForm((prevState) => {
       return {
         ...prevState,
         [e.target.name]: e.target.value,
       };
     });
-  }
+  };
 
-  const handleSubmit = (e) => { // publish page
+  const handleSubmit = (e) => {
+    // publish page
     e.preventDefault();
     createBlogs(form);
     setForm({
-      title: '',
-      body: '',
-      author: ''
+      title: "",
+      body: "",
+      author: "",
     });
   };
 
   return (
-    <section className='container'>
-    <form onSubmit={handleSubmit}>
-      <input
-        type='text'
-        name='title'
-        value={form.title}
-        onChange={handleChange}
-        placeholder='Title'
-      />
-      <input
-        type='text'
-        name='body'
-        value={form.body}
-        onChange={handleChange}
-        placeholder='Body'
-      />
-      <input
-        type='text'
-        name='author'
-        value={form.author}
-        onChange={handleChange}
-        placeholder='Author'
-      />
-      <input type='submit' value='Publish' />
-    </form>
-    
-  </section>
-  )
+    <section className="container mx-auto p-4">
+      <form onSubmit={handleSubmit} className="max-w-lg mx-auto">
+        <div className="mb-4">
+          <lable htmlFor="title" className="block text-gray-700 font bold mb-2">
+            Title
+          </lable>
+        <input
+          type="text"
+          name="title"
+          value={form.title}
+          onChange={handleChange}
+          placeholder="Title"
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        />
+        </div>
+        <div className="mb-4">
+          <lable htmlFor="body" className="block text-gray-700 font bold mb-2">
+            Body
+          </lable>
+        <input
+          type="text"
+          name="body"
+          value={form.body}
+          onChange={handleChange}
+          placeholder="Body"
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        />
+        </div>
+        <div className="mb-4">
+          <lable htmlFor="author" className="block text-gray-700 font bold mb-2">
+            Author
+          </lable>
 
+        <input
+          type="text"
+          name="author"
+          value={form.author}
+          onChange={handleChange}
+          placeholder="Author"
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        />
+        </div>
+        <div className=" flex items-center justify-center">
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        <input type="submit" value="Publish"
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        />
+        </div>
+      </form>
+    </section>
+  );
 }
-
-
