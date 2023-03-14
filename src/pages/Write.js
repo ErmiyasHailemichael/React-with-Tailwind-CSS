924c30ee91ea31ba84e175779d7bb83f6223fae2
Ermiyas H <Ermi@Ermiyass-MacBook-Pro-2.local>
2023-03-13 21:41:14 -0700

    Tailwind: Publish color changed to black

--- src/pages/Write.js
+++ src/pages/Write.js
@@ -74,7 +74,7 @@ export default function Write({ createBlogs }) {
         <div className=" flex items-center justify-center">
 
         <input type="submit" value="Publish"
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        className="bg-black hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
         />
         </div>
       </form>
