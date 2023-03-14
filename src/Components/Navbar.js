009852a2cf1e07de0f03a37802742a1987be8dcd
Ermiyas H <Ermi@Ermiyass-MacBook-Pro-2.local>
2023-03-13 23:14:32 -0700

    Tailwind: Navbar is styled

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,32 +1,34 @@
-import {Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
-    return (
-        <>
-        <nav className=' flex items-center justify-between flex-wrap bg-teal-500 p-6'>
-            <div className='flex items-center flex-shrink-0 text-white mr-6'>
+  return (
+    <nav className="flex  justify-between  bg-blue-500 p-6">
+      <div className="flex items-center flex-shrink-0 text-white mr-6">
         <Link to="/">
-            <button className='font-semibold text-xl tracking-tight'>Home</button>
+          <button className="font-semibold  tracking-tight">Home</button>
         </Link>
+      </div>
+      <div className="block lg:hidden">
+        <button
+          className="flex px-3 py-2 border rounded text-teal-200 hover:text-white hover:border-white"
+          type="button"
+        >
+            <title>Menu</title>
+        </button>
+      </div>
+      <div className="w-full block lg:flex lg:items-center lg:w-auto">
+        <div className="text-sm  flex justify-between">
+         
+          <Link
+            to="/write"
+            className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+          >
+            Write
+          </Link>
         </div>
-
-        <div className='block lg:hidden'>
-            <button className='flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white'
-                type="button"
-                >
-        <Link to="/about">
-            <button>About</button>
-        </Link>
-        <Link to="/contact">
-            <button>Contact</button>
-        </Link>
-        <Link to="/write">
-            <button>Write</button>
-        </Link>
-        </nav>
-        
-        </>
-    )
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
