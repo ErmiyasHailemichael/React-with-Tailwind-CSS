af07dbe210e441529d425de97b116ccf3a9298d4
Ermiyas H <Ermi@Ermiyass-MacBook-Pro-2.local>
2023-03-13 21:20:55 -0700

    tailwind is working

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -5,68 +5,74 @@ import Show from "../pages/Show";
 import Write from "../pages/Write";
 
 const Main = () => {
-    const [blogs, setBlogs] = useState([]);
-    
-    const url = `https://final-jwwb.onrender.com/blogs/`;
-    const navigate = useNavigate();
-    const getBlogs = async () => {
-        const response = await fetch(url);
-        const data = await response.json();
-        setBlogs(data);
-    };
-    
-    const createBlogs = async (blog) => {
-        await fetch(url, {
-        method: "POST",
-        headers: {
-            "Content-Type": "Application/json",
-        },
-        body: JSON.stringify(blog),
-        });
-        getBlogs();
-        navigate("/")
-    };
-    
-    const updateBlogs = async (blog, id) => {
-        await fetch(url + id + "/", {
-        method: "put",
-        headers: {
-            "Content-Type": "Application/json",
-        },
-        body: JSON.stringify(blog),
-        });
-        getBlogs();
-    };
-    
-    const deleteBlogs = async (id) => {
-        await fetch(url  + id + "/", {
-        method: "delete",
-        });
-        getBlogs();
-    };
-    useEffect(() => { getBlogs() }, []);
-    return (
-        <main className="p-4"> 
-        <Routes>
-            <Route 
-            exact path="/"
-            element={<Index
-            blogs={blogs} 
-            createBlogs={createBlogs} />} />
-            <Route
-            path="/blog/:id" element={<Show
-            blogs={blogs}
-            updateBlogs={updateBlogs}
-            deleteBlogs={deleteBlogs} />} />
-             <Route
-            path="/write" element={<Write
-            blogs={blogs}
-            createBlogs={createBlogs} /> } />
+  const [blogs, setBlogs] = useState([]);
 
+  const url = `https://final-jwwb.onrender.com/blogs/`;
+  const navigate = useNavigate();
+  const getBlogs = async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    setBlogs(data);
+  };
 
-        </Routes>
-        </main>
-    );
-}
+  const createBlogs = async (blog) => {
+    await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "Application/json",
+      },
+      body: JSON.stringify(blog),
+    });
+    getBlogs();
+    navigate("/");
+  };
+
+  const updateBlogs = async (blog, id) => {
+    await fetch(url + id + "/", {
+      method: "put",
+      headers: {
+        "Content-Type": "Application/json",
+      },
+      body: JSON.stringify(blog),
+    });
+    getBlogs();
+  };
+
+  const deleteBlogs = async (id) => {
+    await fetch(url + id + "/", {
+      method: "delete",
+    });
+    getBlogs();
+  };
+  useEffect(() => {
+    getBlogs();
+  }, []);
+  return (
+    <main className="p-4">
+      <Routes>
+        <Route
+          exact
+          path="/"
+          element={<Index blogs={blogs} 
+          createBlogs={createBlogs} />}
+        />
+        <Route
+          path="/blog/:id"
+          element={
+            <Show
+              blogs={blogs}
+              updateBlogs={updateBlogs}
+              deleteBlogs={deleteBlogs}
+            />
+          }
+        />
+        <Route
+          path="/write"
+          element={<Write blogs={blogs} createBlogs={createBlogs} />}
+        />
+      </Routes>
+    </main>
+  );
+};
 
 export default Main;
